6a29f7eb0582bf40218cce5267953033cb3539e6
Evgenia Gurova <izhenka@gmail.com>
2017-09-10 00:46:00 +0200

    Interface added

--- oblig1/src/main/Main.java
+++ oblig1/src/main/Main.java
@@ -1,8 +1,28 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("!");
+        menu();
     }
+
+
+
+    public static void menu(){
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        while(!input.equals("q")){
+            System.out.println("Type a word:");
+            input = scanner.nextLine();
+            System.out.println("You are a " + input + "!");
+        }
+    }
+
+
+
+
 }
